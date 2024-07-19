44c12cf112430022a3dd90e96b50dd1bb1174674
Pro-Ski <austinkgski@gmail.com>
2024-07-19 14:50:01 -0600

    added routing for pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./pages/Layout";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export default function NavBar() {
+  return (
+    <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
   </React.StrictMode>
 );
 
