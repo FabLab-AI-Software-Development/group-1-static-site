c89ca3df935e814cc957084b3e2ca7de5e9a045b
Pro-Ski <austinkgski@gmail.com>
2024-07-22 14:59:40 -0600

    Added submit button, hoping to test w/ Angel's submit button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,19 @@
-const Home = () => {
-    return <h1>Home</h1>;
-  };
+import React, {useState, useEffect} from 'react';
+import SearchButton from '../components/SearchButton.jsx';
+
+
+function Home() {
+
+  const [count, setCount] = useState(0);
+
+
+  return( <div><h1>Home</h1>
+  <SearchButton onClick={() => setCount(count + 1)}></SearchButton>
+  <p>Conducted {count} searches</p>
+  </div>
+  );
+}
   
+
+
   export default Home;
