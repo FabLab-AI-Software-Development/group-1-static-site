36b8807fc21ba55530878abb5cc9a385c90c3ad5
rebelrouze <avk0527@gmail.com>
2024-07-22 15:42:00 -0600

    Added form functionality to Austin's button counter

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import SearchButton from '../components/SearchButton.jsx';
+import MyForm from '../components/form.jsx';
 
 
 function Home() {
@@ -8,6 +9,7 @@ function Home() {
 
 
   return( <div><h1>Home</h1>
+  <MyForm></MyForm>
   <SearchButton onClick={() => setCount(count + 1)}></SearchButton>
   <p>Conducted {count} searches</p>
   </div>
